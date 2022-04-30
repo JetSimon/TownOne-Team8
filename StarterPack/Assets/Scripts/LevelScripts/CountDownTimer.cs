1f0d39fdb1182a7acfed47f8345caed92a52163b
Richard Jiang <jiang1774624096@gmail.com>
2022-04-30 11:07:54 -0700

    timer done

--- StarterPack/Assets/Scripts/LevelScripts/CountDownTimer.cs
+++ StarterPack/Assets/Scripts/LevelScripts/CountDownTimer.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDownTimer : MonoBehaviour
-
 {
-    float currentTime = 0f;
-    float startingTime = 120f;
+    public Text text;
+    public float startingTime = 120f;
+    float currentTime;
+    int minute;
+    int second;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,18 @@ public class CountDownTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        while(currentTime > 0)
+        {
+
+        }
+        minute = (int)(currentTime / 60);
+        second = (int)(currentTime % 60);
         currentTime -= 1 * Time.deltaTime;
-        
+        text.text = "0" + minute + ":" + (second < 10 ? "0" : "") + second; 
+        if (currentTime <= 0)
+        {
+            text.text = "00:00";
+            print("time is up");
+        }
     }
 }
