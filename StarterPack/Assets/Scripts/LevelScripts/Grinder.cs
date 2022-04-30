f2eb022748b693efa29da0f29110506a0a49e242
Felipe Lara <70984535+Aosiee@users.noreply.github.com>
2022-04-30 16:17:49 -0700

    Added Fully Functional Grinder

--- StarterPack/Assets/Scripts/LevelScripts/Grinder.cs
+++ StarterPack/Assets/Scripts/LevelScripts/Grinder.cs
@@ -1,18 +1,129 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool grinding;
+    [SerializeField] float shrinkRate = 0.95f;
+    [SerializeField] Component activeSprite;
+
+    bool pendingDeath = false;
+
+    private GameObject dyingPlayer; //= collider.GetComponent<ChickenController>().gameObject;
+    [SerializeField] private bool disableCollision;
+
+    public List<GameObject> overlaps;
+
+    private void Awake()
     {
-        
+        if(activeSprite)
+        {
+            activeSprite.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        }
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (overlaps.Count > 0)
+        {
+            CheckOverlaps();
+        }
+
+        if (grinding == true && dyingPlayer)
+        {
+
+            dyingPlayer.GetComponent<ChickenController>().disableMove();
+
+
+            if (activeSprite)
+            {
+                activeSprite.transform.localScale = new Vector3(1, 1, 1);
+            } else
+            {
+                Debug.Log("Please add activeSprite Reference");
+            }
+
+            if (dyingPlayer.transform.localScale.x > 0.1f)
+            {
+                dyingPlayer.transform.localScale = dyingPlayer.transform.localScale * (Time.deltaTime * shrinkRate);
+            } else
+            {
+                dyingPlayer.transform.localScale = dyingPlayer.transform.localScale * 0;
+
+                if(!pendingDeath) 
+                {
+                    pendingDeath = true;
+                    overlaps.Remove(dyingPlayer);
+                    Invoke("killPlayer", 2f);
+                }
+                
+            }
+        }
+
+    }
+
+    private void CheckOverlaps()
+    {
+        if (!disableCollision)
+        {
+            foreach (GameObject overlap in overlaps)
+            {
+                if(overlap.GetComponent<ChickenController>() != null)
+                {
+                    if (grinding == false)
+                    {
+                        dyingPlayer = overlap.gameObject;
+
+                        dyingPlayer.transform.position = this.transform.position;
+                        dyingPlayer.GetComponent<ChickenController>().disableMove();
+
+                        grinding = true;
+                    }
+                    else
+                    {
+                        Debug.Log("Already Grinding");
+                    }
+                }
+            }
+        } 
+        else
+        {
+            
+        }
+    }
+
+    private void killPlayer()
+    {
+        if(dyingPlayer)
+        {
+            dyingPlayer.GetComponent<ChickenController>().Die();
+            dyingPlayer.GetComponent<ChickenController>().enableMove();
+            grinding = false;
+            pendingDeath = false;
+
+            dyingPlayer = null;
+        } else
+        {
+            return;
+        }
+
+        if(activeSprite)
+        {
+            activeSprite.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        overlaps.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        overlaps.Remove(collision.gameObject);
     }
 }
