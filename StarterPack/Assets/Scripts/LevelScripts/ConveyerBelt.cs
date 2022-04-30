9f323e9a8012a46492c8000041d288cdc974a511
Felipe Lara <70984535+Aosiee@users.noreply.github.com>
2022-04-30 12:35:55 -0700

    Fix for Conveyer Direction

--- StarterPack/Assets/Scripts/LevelScripts/ConveyerBelt.cs
+++ StarterPack/Assets/Scripts/LevelScripts/ConveyerBelt.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,56 @@ public class ConveyerBelt : MonoBehaviour
 {
 
     [SerializeField] BoxCollider2D boxCollider;
-    [SerializeField] Vector2 appliedForce;
+
+    //Pivot
+    [SerializeField] Component pivot;
+
+    private Vector2 localAppliedForce;
+
+    
+
+
+
+    private enum enDirection { 
+        Up = 90, 
+        Down = -90, 
+        Left = 180, 
+        Right = 0 };
+
+
+
+
+
+    [SerializeField] private enDirection desiredDirection;
+
+
+
+    [SerializeField] float appliedForce = 25f;
 
     private List<GameObject> activeOverlaps = new List<GameObject>();
-    
+
+    private ConveyerBelt()
+    {
+        Debug.Log("ConveyerBelt");
+        UpdateDirection(desiredDirection);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Setup ConveyerBelt");
+        setDesiredDirectionFromRot();
+
+        //Debug.Log("Setup ConveyerBelt");
         boxCollider.isTrigger = true;
+
+        UpdateDirection(desiredDirection);
+
+        Debug.Log(desiredDirection);
     }
 
+    private void setDesiredDirectionFromRot()
+    {
+    }
 
     // Update is called once per frame
     void Update()
@@ -46,8 +86,41 @@ public class ConveyerBelt : MonoBehaviour
         {
             if (overlapped.GetComponent<Rigidbody2D>())
             {
-                overlapped.GetComponent<Rigidbody2D>().AddForce(appliedForce);
+                overlapped.GetComponent<Rigidbody2D>().AddForce(localAppliedForce);
             }
+        }
+    }
+
+    //Call this function to change direction of conveyer belt
+    private void UpdateDirection(enDirection direction)
+    {
+        Debug.Log("Desired Direction Called");
+        Debug.Log(desiredDirection);
+
+        if (pivot)
+        {
+            pivot.transform.localPosition = new Vector3(0, 0, 0);
+            pivot.transform.localRotation = Quaternion.Euler(0, 0, ((float)direction));
+        }
+
+        if (direction == enDirection.Up)
+        {
+            localAppliedForce.Set(0, appliedForce);
+        }
+        
+        if (direction == enDirection.Down)
+        {
+            localAppliedForce.Set(0, (appliedForce * -1f));
+        }
+
+        if (direction == enDirection.Left)
+        {
+            localAppliedForce.Set((appliedForce * -1f), 0);
+        }
+
+        if (direction == enDirection.Right)
+        {
+            localAppliedForce.Set(appliedForce, 0);
         }
     }
 
