0b3a91b09a6695522fe55966e6f8ceefa28b4a3f
Jet Simon <jetjdsimon@gmail.com>
2022-05-01 09:42:32 -0700

    make victory screen better

--- StarterPack/Assets/Scripts/UI/GameHud.cs
+++ StarterPack/Assets/Scripts/UI/GameHud.cs
@@ -11,18 +11,22 @@ public class GameHud : MonoBehaviour
     [SerializeField]
     Text resultsText;
 
-    public void ShowWinner(int winnerNumber)
+    [SerializeField]
+    Image winnerImage;
+
+    public void ShowWinner(int winnerNumber, Color winnerColor)
     {
         resultsPanel.SetActive(true);
 
         
         resultsText.text = $"Player {winnerNumber} is the winner!";
+        winnerImage.color = winnerColor;
 
         if(winnerNumber == -1)
         {
             resultsText.text = "There was a tie!";
         }
-        
+
         Time.timeScale = 0;
     }
 
