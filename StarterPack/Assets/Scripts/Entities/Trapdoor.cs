b3ee658fd2ae03cd1087b9ec3a8ff74400cbde93
Evannnne <prefsuite@Live.ca>
2022-05-01 15:44:10 -0700

    fixed horrible game breaking bug with pits

---
+++ StarterPack/Assets/Scripts/Entities/Trapdoor.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapdoor : MonoBehaviour
+{
+    bool grinding;
+    [SerializeField] float shrinkRate = 0.95f;
+
+    [SerializeField] float deathWait = 2f;
+
+    bool pendingDeath = false;
+
+    public float interval = 2.0f;
+
+    private GameObject dyingPlayer; //= collider.GetComponent<ChickenController>().gameObject;
+    [SerializeField] private bool disableCollision;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        StartCoroutine(Loop());
+    }
+    private IEnumerator Loop()
+    {
+        while (true)
+        {
+            animator.SetBool("Open", false);
+            yield return new WaitForSeconds(0.2f);
+            disableCollision = true;
+            yield return new WaitForSeconds(interval);
+            animator.SetBool("Open", true);
+            yield return new WaitForSeconds(0.2f);
+            disableCollision = false;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    private void KillPlayer(ChickenController controller)
+    {
+        StartCoroutine(_KillLoop(controller));
+    }
+    private IEnumerator _KillLoop(ChickenController controller)
+    {
+        dyingPlayer = controller.gameObject;
+        dyingPlayer.GetComponent<ChickenController>().disableMove();
+
+        Vector3 initialScale = dyingPlayer.transform.localScale;
+        Vector3 initialPosition = dyingPlayer.transform.position;
+
+        float t = 0;
+        while (t <= 1)
+        {
+            dyingPlayer.transform.localScale = initialScale * Mathf.Lerp(1, 0, Mathf.Pow(t, 0.5f));
+            dyingPlayer.transform.position = Vector3.Lerp(initialPosition, transform.position, Mathf.Pow(t, 0.33f));
+            t += Time.deltaTime / 0.5f;
+            yield return null;
+        }
+        dyingPlayer.GetComponent<ChickenController>().Die();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var chicken = collision.gameObject.GetComponent<ChickenController>();
+        if (chicken != null && !disableCollision)
+            KillPlayer(chicken);
+    }
+}
